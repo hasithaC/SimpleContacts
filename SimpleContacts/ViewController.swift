af8c11a871c4098fe79da04fb6885ef946e37e94
Hasitha Chamupathi <chamupathihasitha@gmail.com>
2025-05-18 22:38:19 +0530

    setup: iOS project using Swift and UIKit

---
+++ SimpleContacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleContacts
+//
+//  Created by Hasitha Chamupathi on 2025-05-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
